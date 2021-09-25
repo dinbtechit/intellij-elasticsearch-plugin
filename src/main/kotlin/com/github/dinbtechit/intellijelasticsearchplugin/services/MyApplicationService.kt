e9562d0b7509d66aab43cec6357a9ba5a57733bd
GitHub Action <action@github.com>
2021-09-25 17:41:01 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/dinbtechit/intellijelasticsearchplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.dinbtechit.intellijelasticsearchplugin.services
+
+import com.github.dinbtechit.intellijelasticsearchplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/dinbtechit/intellijelasticsearchplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/dinbtechit/intellijelasticsearchplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.dinbtechit.intellijelasticsearchplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.dinbtechit.intellijelasticsearchplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
