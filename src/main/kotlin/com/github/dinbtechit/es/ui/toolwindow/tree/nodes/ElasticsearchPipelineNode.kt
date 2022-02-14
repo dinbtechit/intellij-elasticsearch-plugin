7deb1bc85ccd0e137c7eca9156d3079cdf67909c
Dinesh Srinivasan <dinesh.srinivasan@nielsen.com>
2022-02-14 13:10:31 -0400

    Improvements - new icons for pipline

--- src/main/kotlin/com/github/dinbtechit/es/ui/toolwindow/tree/nodes/ElasticsearchPipelineNode.kt
+++ src/main/kotlin/com/github/dinbtechit/es/ui/toolwindow/tree/nodes/ElasticsearchPipelineNode.kt
@@ -25,11 +25,10 @@ class ElasticsearchPipelineNode : ElasticsearchTreeNode<ElasticsearchDocument.Ty
     }
 
     fun loadDocuments() {
-        val obj = this
         CoroutineScope(Dispatchers.IO).launch {
             val client = ElasticsearchHttpClient<CatIngestPipelinesRequest>()
-            val connection = if (obj.parent is ElasticsearchConnectionTreeNode)
-                (obj.parent as ElasticsearchConnectionTreeNode).data else ConnectionInfo()
+            val connection = if (this@ElasticsearchPipelineNode.parent is ElasticsearchConnectionTreeNode)
+                (this@ElasticsearchPipelineNode.parent as ElasticsearchConnectionTreeNode).data else ConnectionInfo()
             val json = client.sendRequest(connection, CatIngestPipelinesRequest())
             val mapper = jacksonObjectMapper()
             val result: Map<String, Any> = mapper.readValue(json)
